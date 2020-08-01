fa26d4dade4feb54ccf93b12aceac3680a1749ef
Sanchari Gautam <sanchari_g@thirdeyedata.io>
2020-08-01 13:38:18 +0530

    Click images and save like camera with spacebar

--- open_cam.py
+++ open_cam.py
@@ -1,10 +1,17 @@
 import cv2
+from PIL import Image
+import matplotlib
+from datetime import datetime
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
+now = datetime.now()
+# print(now)
 
+fno=0
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
+    
 else:
     rval = False
 
@@ -14,5 +21,11 @@ while rval:
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+    elif key == 32: #space to capture image
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        im = Image.fromarray(rgb)
+        im.save("webcam"+str(now)+str(fno)+".jpeg")
+        fno+=1
+
 vc.release()
 cv2.destroyWindow("preview")
